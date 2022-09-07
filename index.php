a856392358cef26180650ca05bde3a38b0be3856
Faris Adinel <m.farisadinel97@gmail.com>
2022-09-07 15:58:48 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore odit accusamus, ab, optio, ipsam officia officiis libero distinctio veritatis eum tenetur fugit! Cum quaerat ut adipisci, necessitatibus facere deserunt eius.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
